c809c08d3fa87e9dd759eb6af154044d9b0f69cb
HenryDiesel <henry.diesel@ssa.co.za>
2019-06-04 17:19:03 +0200

    Almost final version commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,11 +11,14 @@ angular.module('bitcoinprimeApp')
   .controller('MainCtrl', function($scope, $mdToast, $mdDialog, $http) {
 
     $scope.model = {};
+    $scope.model.maxDate = new Date();
     $scope.model.testField = "TEST VALUE";
+
     //code here
 
 
     $scope.searchBitcoin = function() {
+
       //console.log("button press");
       console.log("start date: " + $scope.model.startDate);
       console.log("end date: " + $scope.model.endDate);
@@ -24,6 +27,7 @@ angular.module('bitcoinprimeApp')
       var startUnix = moment($scope.model.startDate).valueOf();
       var endUnix = moment($scope.model.endDate).valueOf();
 
+
       console.log(startUnix + " - " + endUnix);
       var urlCall = 'https://cors-anywhere.herokuapp.com/https://api-pub.bitfinex.com/v2/candles/trade:1D:tBTCUSD/hist?start=' + startUnix + '&end=' + endUnix + '&sort=1';
       console.log(urlCall);
@@ -31,28 +35,66 @@ angular.module('bitcoinprimeApp')
       $http({
         method: 'GET',
         url: urlCall,
-        withCredentials: false //'https://api.bitfinex.com/v2/candles/trade:1h:tBTCUSD/hist?start=1514764800000&sort=1',
-        /*header : {'Access-Control-Allow-Origin' : '*',
-				'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Headers': "*"
-  }*/
+        withCredentials: false
       }).then(function successCallback(response) {
         console.log("response: " + response.data.length);
         //  console.log(response.data[0][1]);
         var bitArr = response.data;
         var bitData = [];
+
         for (var i = 0; i < bitArr.length; i++) {
           //console.log(i);
+          var primeArr = [];
+          var totalPrime = 0;
+          var priceVal = bitArr[i][1] | 0;
+          var isTotalPrime = "";
+          var rowColor = "";
+
+          var priceString = priceVal + "";
+          var priceNumberArr = priceString.split("");
+
+          console.log(priceString+ " " +priceNumberArr);
+          for(var j = 0;j < priceNumberArr.length; j++)
+          {
+            console.log(priceNumberArr[j]);
+            if(isPrime(priceNumberArr[j]))
+            {
+              primeArr.push(Number(priceNumberArr[j]));
+                totalPrime += Number(priceNumberArr[j]);
+            }
+            else if(priceNumberArr[j] == 1)
+            {
+                primeArr.push(Number(priceNumberArr[j]));
+                totalPrime += Number(priceNumberArr[j]);
+            }
+          }
+          if(isPrime(totalPrime))
+          {
+            isTotalPrime = "Prime";
+            rowColor = "primeBackground";
+          }
+          else {
+            isTotalPrime = "";
+          }
+
           var object = {
             timestamp: bitArr[i][0],
             date: moment(bitArr[i][0]).format("DD MMMM YYYY"),
-            price: bitArr[i][1]
+            price: priceVal,
+            primenumber : primeArr,
+            totalprime : totalPrime,
+            istotalprime : isTotalPrime,
+            rowColor: rowColor
           }
+
           bitData.push(object);
         }
         $scope.model.bitValues = bitData;
         console.log(JSON.stringify(bitData));
+
+
+
+
         // this callback will be called asynchronously
         // when the response is available
 
@@ -67,10 +109,6 @@ angular.module('bitcoinprimeApp')
         ]
         */
 
-
-
-
-
       }, function errorCallback(response) {
         console.log("Error: " + JSON.stringify(response));
         // called asynchronously if an error occurs
@@ -80,8 +118,65 @@ angular.module('bitcoinprimeApp')
 
     }
 
+    //return true if prime
+    function isPrime(input) {
+      let prime = true;
+      for (let i = 2; i <= Math.sqrt(input); i++) {
+        if (input % i == 0) {
+          prime = false;
+          break;
+        }
+      }
+      return prime && (input > 1);
+    }
+
+
+
     $scope.validateDate = function() {
-      showDateValidateMsg("test message!");
+
+      var startDate = $scope.model.startDate;
+      if (startDate) {
+        //$scope.model.minEndDate = moment($scope.model.startDate).add(1, 'days');
+      }
+
+      var endDate = $scope.model.endDate; {
+
+      }
+
+      if (moment(startDate).isAfter(moment(), 'day') || moment(endDate).isAfter(moment(), 'day')) {
+        showDateValidateMsg("You cannot choose a date thats later than today.");
+      }
+      if ((startDate && endDate) && moment(startDate).isSame(moment(endDate), 'day')) {
+
+        showDateValidateMsg("Start and end date cannot be on the same day.");
+        $scope.model.endDate = null;
+
+      }
+      if (moment(startDate).isAfter(moment(endDate), 'day') || moment(endDate).isBefore(moment(startDate), 'day')) {
+
+        showDateValidateMsg("End date must be later than start date");
+        $scope.model.endDate = null;
+
+      }
+      if ((startDate && endDate)) {
+        var diffValue = moment(endDate).diff(moment(startDate), 'months');
+        if (diffValue > 6) {
+          showDateValidateMsg("Date range cannot be more than 6 months");
+          $scope.model.startDate = null;
+          $scope.model.endDate = null;
+        }
+        //console.log(moment(endDate).diff(moment(startDate),'months'));
+      }
+
+
+
+      /*** Test for:
+          Is date newer than today.
+          Is start date later than end Date
+          Is end date earlier than start date
+          Is the difference more than 6 months
+      ***/
+
     }
 
     function showDateValidateMsg(msg) {
